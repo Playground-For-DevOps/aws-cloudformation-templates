aa0707e3627db1c69632edd7ee3f7b0ade401457
ntnguyencse <ntnguyencse@gmail.com>
2024-01-23 08:13:11 +0000

    Add cloudformation file

---
+++ self-test/script.sh
@@ -0,0 +1,9 @@
+aws cloudformation validate-template --template-body file:///home/ubuntu/aws/aws-cloudformation-
+templates/self-test/EKS.yaml
+
+
+
+aws cloudformation create-stack --stack-name test --template-body file:///home/ubuntu/aws/aws-cloudformation-templates/self-test/EKS.yaml --on-failure DELETE 
+--tags "ENV:Nephio-test"
+aws cloudformation delete-stack --stack-name test
+
